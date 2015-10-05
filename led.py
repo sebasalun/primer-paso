d60edadda87344a2a84f13fea6f1208c9c882b15
sebasalun <sebastian@alunideas.com>
2015-10-05 08:52:40 -0500

    Update led.py

--- led.py
+++ led.py
@@ -1,16 +1,16 @@
-led.py
+led.py #Nombre del documento
 #Led para raspberry en python
 
-import time
-import RPi.GPIO as GPIO
+import time # Libreria usada en los delays
+import RPi.GPIO as GPIO #Libreria para activar los puertos de la raspberry
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setmode(GPIO.BCM) #Protocolo para el uso de los puertos
+GPIO.setup(4, GPIO.OUT) #Declaracion de puerto 4 como salida
 
-while:
-	GPIO.output(4, GPIO.HIGH)
-	time.sleep(2)
-	GPIO.output(4, GPIO.LOW)
-	time.sleep(2)
-return
+while: #Ciclo
+	GPIO.output(4, GPIO.HIGH) #Prendemos el led
+	time.sleep(2) #2 segundo prendido
+	GPIO.output(4, GPIO.LOW) #Apaga led
+	time.sleep(2) #2 segundos apagado
+	return # Vuelve a la primera linea del ciclo
